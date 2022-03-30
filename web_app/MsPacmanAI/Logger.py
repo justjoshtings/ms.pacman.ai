27f643759ea22bd7a21655c3b83cbadbca34e3e1
justjoshtings <joshting1@gmail.com>
2022-03-29 23:03:56 -0400

    updates

--- web_app/MsPacmanAI/Logger.py
+++ web_app/MsPacmanAI/Logger.py
@@ -11,7 +11,15 @@ import logging.handlers
 from datetime import datetime
 
 class MyLogger:
+    '''
+    Add logging object for logging.
+    '''
     def __init__(self, log_filename):
+        '''
+        Params:
+            self: instance of object
+            log_filename (str): path to logging file name
+        '''
         # Set up a specific logger with our desired output level
         self.my_logger = logging.getLogger('my_logger')
         self.my_logger.setLevel(logging.DEBUG)
@@ -23,7 +31,22 @@ class MyLogger:
         self.my_logger.addHandler(handler)
     
     def get_mylogger(self):
+        '''
+        Get logger object
+
+        Params:
+            self: instance of object
+        
+        Return:
+            self.my_logger (logger object): returns the logger object
+        '''
         return self.my_logger
 
     def goodbye(self):
+        '''
+        Last log before exiting program
+
+        Params:
+            self: instance of object
+        '''
         self.my_logger.info(f'{datetime.now()} -- Exiting program...')
